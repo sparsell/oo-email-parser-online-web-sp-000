ee22fb3a7daba662aeb1d94ac590126f662cf1c9
Sonja Parsell <hello@sparsell.com>
2020-04-27 14:17:59 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,7 +6,7 @@
 class EmailAddressParser
   attr_accessor :email_addresses
 
-  
+
   def parse(email_addresses)
     emails = email_addresses.split(",") #|| email_addresses.split(" ")
     address = emails.collect do |name|
