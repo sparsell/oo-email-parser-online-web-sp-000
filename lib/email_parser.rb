82df487eb7e4bc96dff1abf3659a27bc5a2a01a4
Sonja Parsell <hello@sparsell.com>
2020-04-27 15:44:36 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,18 +4,16 @@
 # or whitespace (' ').
 
 class EmailParser
+attr_accessor :emails 
 
-  attr_accessor :emails
+def initialize(emails)
+  @emails = emails
+end 
 
-  def initialize(emails)
-    @emails = emails
-  end
-
-  def parse
-    email_array = []
-    email_array = @emails.split(/[,\s]/)
-    email_array = email_array.reject { |e| e == ""}
-    email_array.uniq
-  end
+def parse
+  puts splitEmail = emails.split.collect {|address| address.split(',')} 
+  returnArr = splitEmail.flatten.uniq
+  returnArr
+end
 
 end
